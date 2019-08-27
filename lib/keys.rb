eb979031884b8af8c6f922153af7ea55e5aae974
Alfredo Diaz <mail4alfredo@gmail.com>
2019-08-27 04:18:12 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -6,9 +6,8 @@ require 'pry'
 #  vegetable: ["carrots", "broccoli", "cucumbers"],
 #  meat: ["chicken", "steak", "salmon"],
 #  grains: ["rice", "pasta"]
- }
+# }
 
 def get_the_keys(groceries)
-  #code your solution here!
-  groceries.keys
+  groceries.keys.flatten
 end
